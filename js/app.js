4c838ba78e287a31c65883eb70cd65c387808408
engrmafzaalch <mafzaal786hr@gmail.com>
2019-11-22 16:45:03 +0500

    Link shorter websit without name and logo

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var optional = document.querySelector("#optional");
+var outputField = document.querySelector(".output-field");
+document.querySelector("#form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  document.querySelector(".display").style.display = "block";
+  var optional = document.querySelector("#optional");
+  var outputField = document.querySelector(".output-field");
+  outputField.value = `http://shorten.rigrex.com/${optional.value}`;
+  optional.value = "";
+  document.querySelector(".userLink").value = "";
+  copy();
+});
+document.querySelector(".btn-copy").addEventListener("click", function() {
+  copy();
+});
+function copy() {
+  outputField.select();
+  outputField.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  optional.focus();
+  outputField.blur();
+  copied();
+}
+function copied() {
+  let msg = document.querySelector(".msg");
+  msg.style.display = "block";
+  setTimeout(function() {
+    msg.style.display = "none";
+  }, 4000);
+}
